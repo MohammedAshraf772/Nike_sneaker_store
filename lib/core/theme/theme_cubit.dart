02af44fd4fa30593b4b68d53bb73fe413e41e0d0
MohammedAshraf772 <mohamed117ashraf@gmail.com>
2026-04-28 14:44:11 +0300

    Reorganize Cubit files and delete old files

--- lib/core/theme/theme_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class ThemeCubit extends Cubit<bool> {
-  // true = dark mode, false = light mode
-  ThemeCubit() : super(true);
-
-  void toggleTheme() => emit(!state);
-}
