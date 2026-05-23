6acbb06409eee76a57dfb57ae16017a05d2d3f9a
MohammedAshraf772 <mohamed117ashraf@gmail.com>
2026-05-24 01:05:48 +0300

    Add search and category filtering to home screen

--- lib/features/home/presentation/cubit/home_cubit.dart
+++ lib/features/home/presentation/cubit/home_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:nike_sneaker_store/features/home/data/models/product_model.dart';
+
 import '../../data/repo/product_repository.dart';
 
 part 'home_state.dart';
@@ -20,8 +22,11 @@ class HomeCubit extends Cubit<HomeState> {
       emit(
         HomeSuccess(
           products: products,
+
           allProducts: products,
+
           categories: categories,
+
           selectedCategory: 'All',
         ),
       );
@@ -34,30 +39,76 @@ class HomeCubit extends Cubit<HomeState> {
     final currentState = state;
 
     if (currentState is HomeSuccess) {
-      if (category == 'All') {
-        emit(
-          HomeSuccess(
-            products: currentState.allProducts,
-            allProducts: currentState.allProducts,
-            categories: currentState.categories,
-            selectedCategory: category,
-          ),
-        );
-      } else {
-        final filtered =
-            currentState.allProducts
-                .where((p) => p.category == category)
-                .toList();
+      List<ProductModel> filteredProducts = [];
 
-        emit(
-          HomeSuccess(
-            products: filtered,
-            allProducts: currentState.allProducts,
-            categories: currentState.categories,
-            selectedCategory: category,
-          ),
-        );
+      if (category == 'All') {
+        filteredProducts = currentState.allProducts;
+      } else {
+        filteredProducts =
+            currentState.allProducts
+                .where((product) => product.category == category)
+                .toList();
       }
+
+      emit(
+        HomeSuccess(
+          products: filteredProducts,
+
+          allProducts: currentState.allProducts,
+
+          categories: currentState.categories,
+
+          selectedCategory: category,
+        ),
+      );
+    }
+  }
+
+  void searchProducts(String query) {
+    final currentState = state;
+
+    if (currentState is HomeSuccess) {
+      List<ProductModel> filteredProducts = [];
+
+      if (query.isEmpty) {
+        if (currentState.selectedCategory == 'All') {
+          filteredProducts = currentState.allProducts;
+        } else {
+          filteredProducts =
+              currentState.allProducts
+                  .where(
+                    (product) =>
+                        product.category == currentState.selectedCategory,
+                  )
+                  .toList();
+        }
+      } else {
+        filteredProducts =
+            currentState.allProducts.where((product) {
+              final matchesSearch = product.title.toLowerCase().contains(
+                query.toLowerCase(),
+              );
+
+              final matchesCategory =
+                  currentState.selectedCategory == 'All'
+                      ? true
+                      : product.category == currentState.selectedCategory;
+
+              return matchesSearch && matchesCategory;
+            }).toList();
+      }
+
+      emit(
+        HomeSuccess(
+          products: filteredProducts,
+
+          allProducts: currentState.allProducts,
+
+          categories: currentState.categories,
+
+          selectedCategory: currentState.selectedCategory,
+        ),
+      );
     }
   }
 }
--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:nike_sneaker_store/core/contants/app_colors.dart';
+
 import 'package:nike_sneaker_store/core/widgets/product_card.dart';
 import 'package:nike_sneaker_store/core/widgets/side_menu.dart';
+
 import 'package:nike_sneaker_store/features/cart/screens/cart_screen.dart';
+
 import 'package:nike_sneaker_store/features/home/data/models/product_model.dart';
 import 'package:nike_sneaker_store/features/home/data/repo/product_repository.dart';
+
 import 'package:nike_sneaker_store/features/home/presentation/cubit/home_cubit.dart';
+
 import 'package:nike_sneaker_store/features/home/presentation/screens/product_detail_screen.dart';
+
 import 'package:nike_sneaker_store/features/profile/presentation/screens/profile_screen.dart';
+
 import 'package:shimmer/shimmer.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -18,6 +26,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (_) => HomeCubit(ProductRepository())..getProducts(),
+
       child: const _HomeView(),
     );
   }
@@ -32,12 +41,22 @@ class _HomeView extends StatelessWidget {
       drawer: const SideMenu(),
 
       backgroundColor: AppColors.background,
+
       body: SafeArea(
         child: BlocBuilder<HomeCubit, HomeState>(
           builder: (context, state) {
-            if (state is HomeLoading) return _buildLoading();
-            if (state is HomeError) return _buildError(context, state.message);
-            if (state is HomeSuccess) return _buildContent(context, state);
+            if (state is HomeLoading) {
+              return _buildLoading();
+            }
+
+            if (state is HomeError) {
+              return _buildError(context, state.message);
+            }
+
+            if (state is HomeSuccess) {
+              return _buildContent(context, state);
+            }
+
             return const SizedBox.shrink();
           },
         ),
@@ -48,20 +67,28 @@ class _HomeView extends StatelessWidget {
   Widget _buildLoading() {
     return GridView.builder(
       padding: const EdgeInsets.all(16),
+
       itemCount: 6,
+
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
+
         mainAxisSpacing: 16,
         crossAxisSpacing: 16,
+
         childAspectRatio: 0.7,
       ),
+
       itemBuilder: (_, __) {
         return Shimmer.fromColors(
           baseColor: Colors.grey[800]!,
+
           highlightColor: Colors.grey[700]!,
+
           child: Container(
             decoration: BoxDecoration(
               color: Colors.grey,
+
               borderRadius: BorderRadius.circular(16),
             ),
           ),
@@ -74,10 +101,15 @@ class _HomeView extends StatelessWidget {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+
         children: [
           Text(message, style: const TextStyle(color: Colors.white)),
+
           ElevatedButton(
-            onPressed: () => context.read<HomeCubit>().getProducts(),
+            onPressed: () {
+              context.read<HomeCubit>().getProducts();
+            },
+
             child: const Text('Try Again'),
           ),
         ],
@@ -89,8 +121,13 @@ class _HomeView extends StatelessWidget {
     return CustomScrollView(
       slivers: [
         _buildAppBar(context),
+
         _buildSearchBar(context),
+
+        _buildCategories(context, state),
+
         _buildSectionTitle('${state.products.length} Products Found'),
+
         _buildProductsGrid(context, state),
       ],
     );
@@ -102,14 +139,19 @@ class _HomeView extends StatelessWidget {
         builder: (context) {
           return Padding(
             padding: const EdgeInsets.all(16),
+
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
               children: [
                 const Text(
                   "Home",
+
                   style: TextStyle(
                     color: Colors.white,
+
                     fontSize: 22,
+
                     fontWeight: FontWeight.bold,
                   ),
                 ),
@@ -118,6 +160,7 @@ class _HomeView extends StatelessWidget {
                   children: [
                     IconButton(
                       icon: const Icon(Icons.menu, color: Colors.white),
+
                       onPressed: () {
                         Scaffold.of(context).openDrawer();
                       },
@@ -125,9 +168,11 @@ class _HomeView extends StatelessWidget {
 
                     IconButton(
                       icon: const Icon(Icons.person, color: Colors.white),
+
                       onPressed: () {
                         Navigator.push(
                           context,
+
                           MaterialPageRoute(
                             builder: (_) => const ProfileScreen(),
                           ),
@@ -140,9 +185,11 @@ class _HomeView extends StatelessWidget {
                         Icons.shopping_cart,
                         color: Colors.white,
                       ),
+
                       onPressed: () {
                         Navigator.push(
                           context,
+
                           MaterialPageRoute(builder: (_) => const CartScreen()),
                         );
                       },
@@ -157,17 +204,88 @@ class _HomeView extends StatelessWidget {
     );
   }
 
-  //  Search Bar
   SliverToBoxAdapter _buildSearchBar(BuildContext context) {
     return SliverToBoxAdapter(
       child: Container(
         margin: const EdgeInsets.all(16),
-        padding: const EdgeInsets.all(16),
+
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+
         decoration: BoxDecoration(
           color: AppColors.card,
+
           borderRadius: BorderRadius.circular(12),
         ),
-        child: const Text('Search...', style: TextStyle(color: Colors.grey)),
+
+        child: TextField(
+          style: const TextStyle(color: Colors.white),
+
+          decoration: const InputDecoration(
+            border: InputBorder.none,
+
+            hintText: 'Search products...',
+
+            hintStyle: TextStyle(color: Colors.grey),
+
+            prefixIcon: Icon(Icons.search, color: Colors.grey),
+          ),
+
+          onChanged: (value) {
+            context.read<HomeCubit>().searchProducts(value);
+          },
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter _buildCategories(BuildContext context, HomeSuccess state) {
+    return SliverToBoxAdapter(
+      child: SizedBox(
+        height: 50,
+
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+
+          itemCount: state.categories.length,
+
+          itemBuilder: (context, index) {
+            final category = state.categories[index];
+
+            final isSelected = category == state.selectedCategory;
+
+            return GestureDetector(
+              onTap: () {
+                context.read<HomeCubit>().filterByCategory(category);
+              },
+
+              child: Container(
+                margin: const EdgeInsets.only(right: 12),
+
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 12,
+                ),
+
+                decoration: BoxDecoration(
+                  color: isSelected ? AppColors.primary : AppColors.card,
+
+                  borderRadius: BorderRadius.circular(30),
+                ),
+
+                child: Text(
+                  category,
+
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
@@ -176,6 +294,7 @@ class _HomeView extends StatelessWidget {
     return SliverToBoxAdapter(
       child: Padding(
         padding: const EdgeInsets.all(16),
+
         child: Text(title, style: const TextStyle(color: Colors.white)),
       ),
     );
@@ -184,15 +303,18 @@ class _HomeView extends StatelessWidget {
   SliverPadding _buildProductsGrid(BuildContext context, HomeSuccess state) {
     return SliverPadding(
       padding: const EdgeInsets.all(16),
+
       sliver: SliverGrid(
         delegate: SliverChildBuilderDelegate((context, index) {
           final ProductModel product = state.products[index];
 
           return ProductCard(
             product: product,
+
             onTap: () {
               Navigator.push(
                 context,
+
                 MaterialPageRoute(
                   builder: (_) => ProductDetailScreen(product: product),
                 ),
@@ -200,10 +322,13 @@ class _HomeView extends StatelessWidget {
             },
           );
         }, childCount: state.products.length),
+
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
+
           mainAxisSpacing: 16,
           crossAxisSpacing: 16,
+
           childAspectRatio: 0.7,
         ),
       ),
