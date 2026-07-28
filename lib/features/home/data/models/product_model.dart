7ff53f53cef8fe62ccabadae8188eb3fffe8cdb0
MohammedAshraf772 <mohamed117ashraf@gmail.com>
2026-07-28 16:43:19 +0300

    Add ProductModel.fromFirestore, remove fakestoreapi JSON parser

--- lib/features/home/data/models/product_model.dart
+++ lib/features/home/data/models/product_model.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
 
 class ProductModel extends Equatable {
@@ -24,6 +25,21 @@ class ProductModel extends Equatable {
     required this.ratingRate,
     required this.ratingCount,
   });
+
+  factory ProductModel.fromFirestore(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>? ?? {};
+
+    return ProductModel(
+      id: int.tryParse(doc.id) ?? doc.id.hashCode,
+      title: data['title'] as String? ?? '',
+      image: data['image'] as String? ?? '',
+      price: (data['price'] as num?)?.toDouble() ?? 0,
+      category: data['category'] as String? ?? '',
+      description: data['description'] as String? ?? '',
+      ratingRate: (data['ratingRate'] as num?)?.toDouble() ?? 0,
+      ratingCount: (data['ratingCount'] as num?)?.toInt() ?? 0,
+    );
+  }
 
   @override
   List<Object?> get props => [id];
